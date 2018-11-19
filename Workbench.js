e844ea1c9377322c3c153a22142cf99965cd26f9
zhaojie <zhaojie.jane@bytedance.com>
2018-11-19 10:04:37 +0800

    init workbench

---
+++ Workbench.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { apis } from 'api/config'
+import { Workbench } from 'byted-antx'
+import utils from 'utils'
+import conf from './conf'
+import Detail from '../Detail/Detail'
+import './Workbench.less'
+
+const {
+  columnsConf,
+  columnsInfo,
+  filterBarOptions,
+  refactorDataInBar,
+  refactorDataInTable,
+  onDrawerOpen,
+  defaultVisibleCols,
+  tabFilterConf
+} = conf
+
+const { setStateKey, apiConfig, setTableConf, setFilterConf } = Workbench
+
+const getColumns = (columnsConf, columnsInfo) => cb => {
+  const _columns = columnsInfo ? columnsInfo(cb) : {}
+  return utils.buildColumns(columnsConf, _columns)
+}
+
+@apiConfig()
+@setStateKey('')
+@setTableConf({
+  defaultVisibleCols,   // 是否开启自定义设置面板
+  sortable: true        // 是否采用pro版的设置面板（支持拖拽排序）
+})
+@setFilterConf({
+  defaultList: 3       // 可接收：true, number, [string,]
+})
+export default class extends React.Component {
+  render () {
+    return (
+      <Workbench
+        dataKey={''}
+        refactorDataInBar={refactorDataInBar}
+        refactorDataInTable={refactorDataInTable}
+        onDrawerOpen={onDrawerOpen}
+        columnsConf={getColumns(columnsConf, columnsInfo)}
+        filterBarConf={filterBarOptions()}
+        tabFilterConf={tabFilterConf}
+        {...this.props}
+      >
+        <Detail />
+      </Workbench>
+    )
+  }
+}
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-export { default } from "./{{template}}"
+import { injectReducer } from 'store/reducers'
+import { injectSagas } from 'store/sagas'
+
+export default store => ({
+  path: 'inquiry',
+  breadcrumbName: '查询工作台',
+  breadcrumbCode: 'breadcrumb.workbench',
+  getComponent (nextState, cb) {
+    Promise.all([import('./Workbench.js')]).then(([Containers]) => {
+      const reducer = Containers.default.reducer
+      const sagas = Containers.default.sagas
+      const key = Containers.default.stateKey
+      injectReducer(store, { key, reducer })
+      injectSagas(store, { key, sagas })
+      /*  Return getComponent   */
+      cb(null, Containers.default)
+    })
+  }
+})
